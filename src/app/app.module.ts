f52f21084aaf2635dd46aef5109ea61a14d8e0a3
Ivan Gavrilov <ops282@mail.ru>
2019-08-20 06:23:06 +0300

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { HTTP } from '@ionic-native/http/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { ToastController } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -19,7 +19,10 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   providers: [
     StatusBar,
+    HTTP,
+    InAppBrowser,
     SplashScreen,
+    ToastController,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HTTP } from '@ionic-native/http/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private http: HTTP, private iab: InAppBrowser) {
+    // tslint:disable-next-line:max-line-length
+    /*this.http.get('https://api.worldoftanks.ru/wot/auth/login/?application_id=8e1ae50869c452ec624476262bb20f0d&expires_at=1566512595&application_id=8e1ae50869c452ec624476262bb20f0d&display=page&nofollow=1', {}, {})
+        .then(data => {
+            this.http.get(JSON.parse(data.data).data.location, {}, {}).then( dt => {
+                console.log(dt);
+            }).catch(error => {
+                console.log('ERROR!');
+                console.log(error.error);
+            });
+        })
+        .catch(error => {
+          console.log('ERROR!');
+          console.log(error.error);
+        });*/
+// tslint:disable-next-line:max-line-length
+    const browser = this.iab.create('https://api.worldoftanks.ru/wot/auth/login/?application_id=8e1ae50869c452ec624476262bb20f0d&expires_at=1566512595&application_id=8e1ae50869c452ec624476262bb20f0d&display=page&redirect_uri=https://google.com', '_blank', {fullscreen: 'yes'});
+    browser.on('loadstart').subscribe(event => {
+        if (event.url.substr(0, event.url.indexOf('?')) === 'https://google.com/') {
+            alert(event.url);
+            // tslint:disable-next-line:max-line-length
+            alert(event.url.substr(event.url.indexOf('access_token=') + 13, event.url.length - event.url.substr(event.url.indexOf('access_token=') + 13, event.url.substr(0, event.url.indexOf('access_token=') + 13).length).length - event.url.substr(event.url.indexOf('&nickname'), event.url.length).length));
+            browser.close();
+        }
+    });
+  }
 
 }
