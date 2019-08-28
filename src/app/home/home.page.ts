a4478ec1001ed530638ff7962d79bacff88290a6
renamethis <112321Johnny>
2019-08-28 14:58:47 +0300

    Update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,30 +8,17 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-    token: any;
+    token: string;
+    uri:any;
   constructor(private http: HTTP, private iab: InAppBrowser, private storage: Storage) {
-    // tslint:disable-next-line:max-line-length
-    /*this.http.get('https://api.worldoftanks.ru/wot/auth/login/?application_id=8e1ae50869c452ec624476262bb20f0d&expires_at=1566512595&application_id=8e1ae50869c452ec624476262bb20f0d&display=page&nofollow=1', {}, {})
-        .then(data => {
-            this.http.get(JSON.parse(data.data).data.location, {}, {}).then( dt => {
-                console.log(dt);
-            }).catch(error => {
-                console.log('ERROR!');
-                console.log(error.error);
-            });
-        })
-        .catch(error => {
-          console.log('ERROR!');
-          console.log(error.error);
-        });*/
     storage.get('token').then(value => {
        if (value == null) {
            // tslint:disable-next-line:max-line-length
-           const browser = this.iab.create('https://api.worldoftanks.ru/wot/auth/login/?application_id=8e1ae50869c452ec624476262bb20f0d&expires_at=1566512595&application_id=8e1ae50869c452ec624476262bb20f0d&display=page&redirect_uri=https://google.com', '_blank', {fullscreen: 'yes'});
+           const browser = this.iab.create('https://api.worldoftanks.ru/wot/auth/login/?application_id=8e1ae50869c452ec624476262bb20f0d&expires_at=1568200768&display=page&redirect_uri=https://google.com', '_blank', {fullscreen: 'yes'});
            browser.on('loadstart').subscribe(event => {
                if (event.url.substr(0, event.url.indexOf('?')) === 'https://google.com/') {
                    // tslint:disable-next-line:max-line-length
-                   this.token = event.url.substr(event.url.indexOf('access_token=') + 13, event.url.length - event.url.substr(event.url.indexOf('access_token=') + 13, event.url.substr(0, event.url.indexOf('access_token=') + 13).length).length - event.url.substr(event.url.indexOf('&nickname'), event.url.length).length);
+                   this.token = event.url.substr(event.url.indexOf('access_token') + 13, event.url.indexOf('&nickname')-(event.url.indexOf('access_token')+13));
                    storage.set('token', this.token);
                    browser.close();
                }
@@ -39,6 +26,9 @@ export class HomePage {
        } else {
            this.token = value;
            alert(this.token);
+           this.http.post('https://api.worldoftanks.ru/wot/auth/prolongate/?application_id=8e1ae50869c452ec624476262bb20f0d', {'access_token' : this.token, 'expires_at': '1568200768'}, {'Content-Type': 'application/json'}).then(data => {
+                alert(data.data);
+           });
         }
     });
   }
